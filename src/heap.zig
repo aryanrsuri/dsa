3bbcdaf091d68ca20914d75889159b47bdc31eb1
Aryan Suri <arysuri@proton.me>
2023-06-16 23:00:58 -0700

    remove () for heap, there are issues with updating root elem
    Signed-off-by: Aryan Suri <arysuri@proton.me>

--- src/heap.zig
+++ src/heap.zig
@@ -17,29 +17,26 @@ pub fn Heap(comptime T: type) type {
             self.data.deinit();
         }
 
-        fn heapify(self: *Self, i: usize) void {
-            var max = i;
-            var left = (2 * i) + 1;
-            var right = (2 * i) + 2;
-            var len = self.data.items.len;
-            // if (left >= len or left < 0) {
-            //     return;
-            // }
+        fn heapify(self: *Self, index: usize) void {
+            var max = index;
+            var left = (2 * index) + 1;
+            var right = (2 * index) + 2;
+            const len = self.data.items.len;
+
             if (left < len and self.data.items[left] > self.data.items[max]) {
                 max = left;
             }
             if (right < len and self.data.items[right] > self.data.items[max]) {
                 max = right;
             }
-            if (max != i) {
-                std.mem.swap(T, &self.data.items[i], &self.data.items[max]);
+            if (max != index) {
+                std.mem.swap(T, &self.data.items[index], &self.data.items[max]);
                 self.heapify(max);
             }
         }
 
         fn insert(self: *Self, element: T) void {
-            var len = self.data.items.len;
-            std.debug.print("\nelem: {}\n", .{element});
+            const len = self.data.items.len;
             if (len <= 1) {
                 self.data.append(element) catch {
                     @panic(" Append data failed ");
@@ -52,10 +49,59 @@ pub fn Heap(comptime T: type) type {
                 var i = (len / 2) - 1;
                 if (i == 0) {
                     self.heapify(i);
+                    return;
+                } else {
+                    while (i > 0) : (i -= 1) {
+                        self.heapify(i);
+                    }
+                }
+            }
+        }
+
+        fn remove(self: *Self, element: T) ?T {
+            const len = self.data.items.len;
+            if (len <= 1) {
+                return self.data.popOrNull();
+            } else {
+                const needle = [1]T{element};
+                const index = std.mem.indexOf(
+                    T,
+                    self.data.items,
+                    &needle,
+                );
+                var i = (len / 2) - 1;
+                if (i == 0) {
+                    self.heapify(i);
                 }
                 while (i > 0) : (i -= 1) {
                     self.heapify(i);
                 }
+
+                if (index == null) {
+                    return null;
+                } else {
+                    return self.data.orderedRemove(index.?);
+                }
+            }
+        }
+
+        fn depth(self: *Self, index: usize) usize {
+            var len = self.data.items.len;
+            var curr_depth: usize = 0;
+            var curr_node: usize = index;
+            while (curr_node > 0 and curr_node < len) : (curr_node = (curr_node - 1) / 2) {
+                curr_depth += 1;
+            }
+
+            return curr_depth;
+        }
+
+        fn display(self: *Self) void {
+            const len = self.data.items.len;
+            var iter: usize = 0;
+            std.debug.print("\n-------------------------\n", .{});
+            while (iter < len) : (iter += 1) {
+                std.debug.print("\n [ node: {} ** depth: {}  ] \n", .{ self.data.items[iter], self.depth(iter) });
             }
         }
     };
@@ -68,7 +114,13 @@ test " Heap " {
     h.insert(3);
     h.insert(4);
     h.insert(9);
+    h.insert(8);
+    h.insert(10);
+    h.insert(8);
     h.insert(5);
     h.insert(2);
-    std.debug.print("\n---------------\nheap: {any}\n", .{h.data.items});
+    h.display();
+    _ = h.remove(4);
+    _ = h.remove(1);
+    h.display();
 }
