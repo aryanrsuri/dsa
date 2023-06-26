7574a9702df01227f7f8e4fa008caa349821d28a
aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>
2023-06-25 20:41:12 -0700

    wip qsort
    Signed-off-by: aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>

---
+++ src/sort/quick_sort.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+/// Quick sort is a partition based
+/// algorithim ...
+/// @param { []i64 } array -> array to sort on
+/// @returns { void }
+pub fn quick_sort(array: []i64) void {
+    var pivot = array.len - 1;
+
+    
+}
